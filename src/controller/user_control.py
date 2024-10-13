a9cf8d4736b89b24f17a184864c0c35dca72fce4
Edison <edison100ospina@gmail.com>
2024-10-13 11:43:33 -0500

    avances con el control de usuarios

---
+++ src/controller/user_control.py
@@ -0,0 +1,28 @@
+#aca la logica para implementar el control de usuarios.
+
+def crear_tabla():
+    pass
+
+
+def eliminar_tabla():
+    pass
+
+
+def insertar_usuario():
+    pass
+
+
+def buscar_usuario_por_cedula():
+    pass
+
+
+def eliminar_usuario_por_cedula():
+    pass
+
+
+def actualizar_usuario_por_cedula():
+    pass
+
+
+def obtener_cursor():
+    pass
